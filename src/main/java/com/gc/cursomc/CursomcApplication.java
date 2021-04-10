2ea1f42a13bd901acc067929c5440c3a946f5bd9
Gabriel Cieslak <gabrielcies26@gmail.com>
2021-04-10 15:21:15 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/gc/cursomc/CursomcApplication.java
+++ src/main/java/com/gc/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.gc.cursomc.domain.Cidade;
 import com.gc.cursomc.domain.Cliente;
 import com.gc.cursomc.domain.Endereco;
 import com.gc.cursomc.domain.Estado;
+import com.gc.cursomc.domain.ItemPedido;
 import com.gc.cursomc.domain.Pagamento;
 import com.gc.cursomc.domain.PagamentoComBoleto;
 import com.gc.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.gc.cursomc.repostirories.CidadeRepository;
 import com.gc.cursomc.repostirories.ClienteRepository;
 import com.gc.cursomc.repostirories.EnderecoRepository;
 import com.gc.cursomc.repostirories.EstadoRepository;
+import com.gc.cursomc.repostirories.ItemPedidoRepository;
 import com.gc.cursomc.repostirories.PagamentoRepository;
 import com.gc.cursomc.repostirories.PedidoRepository;
 import com.gc.cursomc.repostirories.ProdutoRepository;
@@ -56,8 +58,8 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private PedidoRepository pedidoRepository;	
 	
-	
-	
+	@Autowired
+	private ItemPedidoRepository itempedidoRepository;	
 	
 	
 	public static void main(String[] args) {
@@ -124,9 +126,18 @@ public class CursomcApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1,p1,0.00,1,2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1,p3,0.00,2,80.00);
+		ItemPedido ip3 = new ItemPedido(ped2,p2,100.00,1,800.00);
 		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
 		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
 		
+		itempedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 	} 
 
 }
